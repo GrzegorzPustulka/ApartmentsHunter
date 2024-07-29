43db9d35f587baf06b6b8741a607f0b3fd09da53
GrzegorzPustulka <grzegorzpustulka@onet.pl>
2024-07-29 22:42:31 +0200

    Catalogue - schema refactoring

--- catalogue/catalogue/schemas/apartments.py
+++ catalogue/catalogue/schemas/apartments.py
@@ -1,30 +1,20 @@
 from bson import ObjectId
-from pydantic import BaseModel, Field, model_validator, field_validator, ValidationInfo
-from typing import TypeAlias, Literal, Self, Any
+from pydantic import BaseModel, Field, model_validator, field_validator
+from catalogue.resources.districts import GET_DISTRICTS
+from typing import Literal
 
 
-districts = {
-    "krakow": [
-        "Bieńczyce",
-        "Bieżanów-Prokocim",
-        "Bronowice",
-        "Czyżyny",
-        "Dębniki",
-        "Grzegórzki",
-        "Krowodrza",
-        "Łagiewniki-Borek Fałęcki",
-        "Mistrzejowice",
-        "Nowa Huta",
-        "Podgórze",
-        "Podgórze Duchackie",
-        "Prądnik Biały",
-        "Prądnik Czerwony",
-        "Stare Miasto",
-        "Swoszowice",
-        "Wzgórza Krzesławickie",
-        "Zwierzyniec",
-    ]
-}
+BuildingType = Literal[
+    "Blok",
+    "Kamienica",
+    "Dom wolnostojący",
+    "Szeregowiec",
+    "Apartamentowiec",
+    "Loft",
+    "Pozostałe",
+]
+NumberOfRooms = Literal["1 pokój", "2 pokoje", "3 pokoje", "4 i więcej"]
+Deposit = Literal["brak informacji", "brak kaucji"]
 
 
 class ApartmentParams(BaseModel):
@@ -35,77 +25,16 @@ class ApartmentParams(BaseModel):
     maximum_price: int | None = None
     minimum_area: int | None = None
     maximum_area: int | None = None
+    maximum_deposit: list[Deposit] | int | None = None
 
-    current: list[str] | None = None
-    heating: list[str] | None = None
-    water: list[str] | None = None
-    gas: list[str] | None = None
-    rubbish: list[str] | None = None
-    internet: list[str] | None = None
-
-    building_type: list[str] | None = None
-    number_of_rooms: list[str] | None = None
+    building_type: list[BuildingType] | None = None
+    number_of_rooms: list[NumberOfRooms] | None = None
     floor_level: list[int] | None = None
     is_furnished: bool | None = None
     is_private_offer: bool | None = None
 
-    @field_validator(
-        "current",
-        "heating",
-        "water",
-        "rubbish",
-        "gas",
-        "internet",
-        "building_type",
-        mode="before",
-    )
-    @classmethod
-    def validate_fields(cls, v: Any, info: ValidationInfo) -> list[str] | int | float:
-        allowed_values = {
-            "current": ["wg zuzycia", "brak informacji", "w cenie czynszu"],
-            "heating": ["wg zuzycia", "w cenie czynszu"],
-            "water": ["wg zuzycia", "w cenie czynszu"],
-            "rubbish": ["brak informacji", "w cenie czynszu"],
-            "gas": ["wg zuzycia", "brak informacji", "w cenie czynszu", "brak gazu"],
-            "internet": ["brak informacji", "w cenie czynszu", "we wlasnym zakresie"],
-            "building_type": [
-                "Blok",
-                "Kamienica",
-                "Dom wolnostojący",
-                "Szeregowiec",
-                "Apartamentowiec",
-                "Loft",
-                "Pozostałe",
-            ],
-        }
-
-        if v:
-            for item in v:
-                if item not in allowed_values[info.field_name]:
-                    raise ValueError(
-                        f"{item} is not a valid value for {info.field_name}. Allowed values are: {allowed_values[info.field_name]}"
-                    )
-
-        return v
-
-    @field_validator("floor_level", mode="before")
-    @classmethod
-    def validate_floor_level(cls, v: Any) -> list[str]:
-        result = []
-        if v:
-            for item in v:
-                if item == 1:
-                    result.append("1 pokój")
-                elif item == 2:
-                    result.append("2 pokoje")
-                elif item == 3:
-                    result.append("3 pokoje")
-                elif item == 4:
-                    result.append("4 i więcej")
-                else:
-                    raise ValueError(f"Invalid value for floor_level: {item}")
-            return result
-        return v
+    bedrooms: list[int] | None = None
+    standard: list[str] | None = None
 
     @model_validator(mode="after")
     def validate_price_and_area(self) -> "ApartmentParams":
@@ -125,16 +54,9 @@ class ApartmentParams(BaseModel):
 
         if self.district and self.city:
             for district in self.district:
-                if district not in districts[self.city]:
-                    raise ValueError(f"District {district} is not supported.")
+                if district not in GET_DISTRICTS[self.city]:
+                    raise ValueError(f"{district} is not supported for {self.city}.")
         return self
-
-
-class Media(BaseModel):
-    current: int | float | str
-    heating: int | float | str
-    water: int | float | str
-    gas: int | float | str
 
 
 class ApartmentRead(BaseModel):
@@ -146,9 +68,6 @@ class ApartmentRead(BaseModel):
     area: int | float
     price: int | float
     floor_level: str | None = None
-    media: Media
-    rubbish: int | float | str
-    internet: int | float | str
     building_type: str
     number_of_rooms: str
     is_furnished: bool
