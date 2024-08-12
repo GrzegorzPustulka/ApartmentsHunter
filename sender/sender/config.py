e681771797831ec313d869527e548397904d5004
GrzegorzPustulka <94792342+GrzegorzPustulka@users.noreply.github.com>
2024-08-12 23:08:32 +0200

    Added new microservice - Sender (#35)

---
+++ sender/sender/config.py
@@ -0,0 +1,30 @@
+from pydantic import SecretStr
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from functools import lru_cache
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env")
+
+    # RabbitMQ
+    rabbit_host: str = "rabbitmq"
+    rabbit_user: str = "guest"
+    rabbit_password: SecretStr = SecretStr("guest")
+    celery_queue: str = "sender"
+    rabbit_url: str = (
+        f"pyamqp://{rabbit_user}:{rabbit_password.get_secret_value()}@{rabbit_host}//"
+    )
+
+    # SMTP
+    smtp_server: str = "localhost"
+    smtp_port: int = 1025
+    sender_email: str = "apartments@hunter.com"
+    smtp_password: SecretStr = SecretStr("")
+
+
+@lru_cache()
+def get_settings():
+    return Settings()
+
+
+settings = get_settings()
---
+++ sender/sender/tasks.py
@@ -0,0 +1,10 @@
+from typing import Any
+from sender.celery_app import celery_app
+from sender.services.email_service import EmailService
+
+
+@celery_app.task(name="process_offer_in_sender")
+def process_offer(offer: dict[str, Any], data: dict[str, str]) -> None:
+    if data["notification_destination"] == "email":
+        sender = EmailService(data["user_email"])
+        sender.send_email(offer)
