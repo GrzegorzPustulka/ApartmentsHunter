904176006ea92a558d9c486daaa045649b3385ba
GrzegorzPustulka <94792342+GrzegorzPustulka@users.noreply.github.com>
2024-08-17 16:40:53 +0200

    Subscriptions - added endpoint for creating jwt token (#41)

--- subscriptions/subscriptions/api/v1/endpoints/users.py
+++ subscriptions/subscriptions/api/v1/endpoints/users.py
@@ -1,10 +1,14 @@
+from fastapi.security import OAuth2PasswordRequestForm
+
 from subscriptions.api.v1.dependencies import get_db
 from sqlalchemy.orm import Session
 from fastapi import APIRouter, Depends, HTTPException, status
-from subscriptions.schemas.users import UserCreate
+from subscriptions.schemas.users import UserCreate, Token
 from subscriptions.repository.users import user as user_repository
-
+from subscriptions.core.config import settings
+from subscriptions.core.security import create_access_token
 from typing import Annotated
+from datetime import timedelta
 
 router = APIRouter(prefix="/users", tags=["users"])
 
@@ -22,3 +26,20 @@ async def create_offer(user_in: UserCreate, db: Session):
         )
 
     user_repository.create(db, user_in)
+
+
+@router.post("/access-token")
+def login_access_token(
+    db: Session, form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
+) -> Token:
+    user = user_repository.authenticate(
+        db, email=form_data.username, password=form_data.password
+    )
+
+    if not user:
+        raise HTTPException(status_code=400, detail="Incorrect email or password")
+
+    access_token_expires = timedelta(minutes=settings.access_token_expire_minutes)
+    return Token(
+        access_token=create_access_token(user.id, expires_delta=access_token_expires)
+    )
--- subscriptions/subscriptions/repository/users.py
+++ subscriptions/subscriptions/repository/users.py
@@ -2,20 +2,28 @@ from sqlalchemy.orm import Session
 from sqlalchemy import select
 from subscriptions.models import User
 from subscriptions.schemas.users import UserCreate, UserUpdate
-from subscriptions.core.security import get_password_hash
+from subscriptions.core.security import get_password_hash, verify_password
 from subscriptions.repository.base import BaseRepository
 
 
 class UserRepository(BaseRepository[User, UserCreate, UserUpdate]):
     @staticmethod
     def get_by_email(db: Session, email: str) -> User | None:
-        return db.execute(select(User).where(User.email == email)).first()
+        return db.execute(select(User).where(User.email == email)).scalars().first()
 
     def create(self, db: Session, user_in: UserCreate) -> None:
         user_in = UserCreate(
             email=user_in.email, password=get_password_hash(user_in.password)
         )
         super().create(db=db, obj_in=user_in)
+
+    def authenticate(self, db: Session, email: str, password: str) -> User | None:
+        db_user = self.get_by_email(db, email)
+        if not db_user:
+            return None
+        if not verify_password(password, db_user.password):
+            return None
+        return db_user
 
 
 user = UserRepository(User)
