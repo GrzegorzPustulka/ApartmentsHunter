775457df6b68d12bee7694c726f9b3b74781ac96
GrzegorzPustulka <grzegorzpustulka@onet.pl>
2024-08-17 18:51:23 +0200

    Subscriptions - added endpoint for deleting subscriptions and displaying all subscriptions

--- subscriptions/subscriptions/api/v1/endpoints/subscriptions.py
+++ subscriptions/subscriptions/api/v1/endpoints/subscriptions.py
@@ -48,3 +48,40 @@ async def read_subscription(
         )
 
     return SubscriptionRead.model_validate(sub.as_dict())
+
+
+@router.get(
+    "/",
+    response_model=list[SubscriptionRead],
+    status_code=status.HTTP_200_OK,
+)
+async def read_all_subscriptions(
+    current_user: CurrentUser, db: DB
+) -> list[SubscriptionRead]:
+    subs = sub_repository.get_by_user_id(db, current_user.id)
+
+    if not subs:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="You do not have subscriptions.",
+        )
+    return [SubscriptionRead.model_validate(sub.as_dict()) for sub in subs]
+
+
+@router.delete("/{subscription_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_subscription(current_user: CurrentUser, subscription_id: UUID, db: DB):
+    sub = sub_repository.get_by_id(db, subscription_id)
+
+    if not sub:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Subscription with id '{subscription_id}' does not exist.",
+        )
+
+    if sub.user_id != current_user.id:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="You do not have permission to delete this subscription.",
+        )
+
+    sub_repository.delete_by_id(db, subscription_id)
