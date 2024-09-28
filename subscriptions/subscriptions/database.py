b4fb32617d2f9422788075d614e12386213ceeb1
GrzegorzPustulka <94792342+GrzegorzPustulka@users.noreply.github.com>
2024-09-28 15:46:48 +0200

    writer - notify/listen (#73)

--- subscriptions/subscriptions/database.py
+++ subscriptions/subscriptions/database.py
@@ -2,11 +2,7 @@ from sqlalchemy import create_engine, event
 from sqlalchemy.orm import sessionmaker
 
 from subscriptions.core.config import settings
-from subscriptions.models import Subscription
-from writer.events import receive_after_insert
 
 engine = create_engine(settings.sqlalchemy_database_uri)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, future=True)
-
-event.listen(Subscription, "after_insert", receive_after_insert)
---
+++ writer/writer/main.py
@@ -0,0 +1,4 @@
+from writer.worker import listen_notifications
+
+if __name__ == "__main__":
+    listen_notifications()
